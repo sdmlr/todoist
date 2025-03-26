6f9c5e388cbf49b9a7f7a3b89e397fcac7b9588c
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-26 16:40:44 +0000

    successful inegration for auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
-import { Stack } from "expo-router";
-import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { Stack, usePathname, useRouter, useSegments } from "expo-router";
+import { ClerkProvider, ClerkLoaded, useAuth } from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
 import { tokenCache } from "@/utils/cache";
 import { Colors } from "@/constants/Colors";
+import { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
@@ -13,8 +15,38 @@ if (!publishableKey) {
 }
 
 const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const router = useRouter();
+  const segments = useSegments();
+  const pathName = usePathname();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+    console.log('segments', segments);
+    console.log('pathName', pathName);
+    
+
+    const inAuthGroup = segments[0] === "(authenticated)";
+
+    if (isSignedIn && !inAuthGroup) {
+      router.replace("/(authenticated)/(tabs)/today");
+    } else if (!isSignedIn && pathName === '/') {
+      router.replace("/");
+    }
+  }, [isLoaded, isSignedIn]);
+
+  if (!isLoaded) {
+    return (
+      <View className="flex justify-center items-center">
+        <ActivityIndicator size={large} color="#dc4c3e" />
+      </View>
+    );
+  }
+
   return (
-    <Stack screenOptions={{ contentStyle: { backgroundColor: Colors.primary}}}>
+    <Stack
+      screenOptions={{ headerShown: false, contentStyle: { backgroundColor: Colors.primary } }}
+    >
       <Stack.Screen
         name="index"
         options={{
