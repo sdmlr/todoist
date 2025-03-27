1b7dccb92589b7e827ff84c24fa6e7a3638bf6f4
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-27 12:08:41 +0000

    browse header added

--- app/(authenticated)/(tabs)/browse/_layout.tsx
+++ app/(authenticated)/(tabs)/browse/_layout.tsx
@@ -1,24 +1,36 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { Colors } from "@/constants/Colors";
-import MoreButton from "@/components/MoreButton"; // Example custom button
+import { useUser } from "@clerk/clerk-expo";
+import { Image } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function BrowseStackLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShadowVisible: false,
-        contentStyle: { backgroundColor: Colors.background },
-      }}
-    >
+    <Stack screenOptions={{
+      headerShadowVisible: false,
+      contentStyle: { backgroundColor: Colors.backgroundAlt}
+    }}>
       <Stack.Screen
         name="index"
         options={{
           title: "Browse",
-          headerLargeTitle: true,
-          headerRight: () => <MoreButton />,
+          headerLeft: () => <HeaderLeft />,
+          headerRight: () => <HeaderRight />
         }}
       />
-      {/* If you had deeper routes like "details.tsx", you'd add them here too */}
     </Stack>
   );
+}
+
+const HeaderLeft = () => {
+  const { user } = useUser();
+  return <Image source={{ uri: user?.imageUrl }} style={{ width: 32, height: 32, borderRadius: 16 }} />
+}
+
+const HeaderRight = () => {
+  return (
+    // <Link href="/browse/settings">
+      <Ionicons name="settings-outline" size={24} color={Colors.primary}/>
+    // </Link>
+  )
 }
