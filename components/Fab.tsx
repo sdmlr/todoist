468c0683dc09842ca2ef4c0e71a8f785544c9fe5
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-27 12:07:33 +0000

    component set up

---
+++ components/Fab.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Fab = () => {
+  return (
+    <View>
+      <Text>Fab</Text>
+    </View>
+  )
+}
+
+export default Fab
