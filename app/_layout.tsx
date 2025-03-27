fa32feafce29f3912496cec0a1b140f2b0b3aab5
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-27 12:12:03 +0000

    corrected condition

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,7 @@ const InitialLayout = () => {
 
     if (isSignedIn && !inAuthGroup) {
       router.replace("/(authenticated)/(tabs)/today");
-    } else if (!isSignedIn && pathName === '/') {
+    } else if (!isSignedIn && pathName !== '/') {
       router.replace("/");
     }
   }, [isLoaded, isSignedIn]);
