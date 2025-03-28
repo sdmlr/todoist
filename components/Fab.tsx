eae03aafcf8c9a0b9c1ee28d2b85f89dd2859510
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-28 02:23:11 +0000

    builds component

--- components/Fab.tsx
+++ components/Fab.tsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { toast } from "sonner-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Colors } from "@/constants/Colors";
+import { useRouter } from "expo-router";
+import * as Haptics from 'expo-haptics'
 
 const Fab = () => {
-  return (
-    <View>
-      <Text>Fab</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Fab
+  const onPress = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+    router.push('/task/new')
+  };
+
+  return (
+    <TouchableOpacity
+      // className="absolute bottom-5 right-5 z-[1000] bg-primary p-2.5 rounded-full items-center justify-center"
+      onPress={onPress}
+      style={{
+        position: "absolute",
+        bottom: 24,
+        right: 24,
+        zIndex: 1000,
+        height: 56,
+        width: 56,
+        backgroundColor: Colors.primary,
+        padding: 10,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+      }}
+    >
+      <Ionicons name="add" size={28} color="white" />
+    </TouchableOpacity>
+  );
+};
+
+export default Fab;
--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import * as DropdownMenu from "zeego/dropdown-menu";
+import * as Clipboard from "expo-clipboard";
+import { toast } from "sonner-native";
+import { ToastAndroid } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Colors } from "@/constants/Colors";
 
-const MoreButton = () => {
+type MoreButtonProps = {
+  pageName: string;
+};
+
+const MoreButton = ({ pageName }: MoreButtonProps) => {
+  const copyToClipboard = async () => {
+    const path = `todoist://(authenticated)/(tabs)/${pageName.toLocaleLowerCase()}`;
+    await Clipboard.setStringAsync(path);
+    ToastAndroid.show("Coppied to clipboard", ToastAndroid.SHORT);
+    toast.success("Coppied to clipboard");
+  };
   return (
-    <View>
-      <Text>MoreButton</Text>
-    </View>
-  )
-}
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <TouchableOpacity className="">
+          <Ionicons name="ellipsis-horizontal-outline" size={39} color={Colors.primary} />
+        </TouchableOpacity>
+      </DropdownMenu.Trigger>
 
-export default MoreButton
+      <DropdownMenu.Content>
+        <DropdownMenu.Item key="link" onSelect={copyToClipboard}>
+          <DropdownMenu.ItemTitle>Copy</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemIcon
+            ios={{
+              name: "link",
+              pointSize: 24,
+            }}
+          ></DropdownMenu.ItemIcon>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+
+      <DropdownMenu.Group>
+        <DropdownMenu.Item key="select">
+          <DropdownMenu.ItemTitle>Select Tasks</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemIcon
+            ios={{
+              name: "square.stack",
+              pointSize: 24,
+            }}
+          ></DropdownMenu.ItemIcon>
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item key="view">
+          <DropdownMenu.ItemTitle>View</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemIcon
+            ios={{
+              name: "slider.horizontal.3",
+              pointSize: 24,
+            }}
+          ></DropdownMenu.ItemIcon>
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Item key="activity">
+          <DropdownMenu.ItemTitle>Activity Log</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemIcon
+            ios={{
+              name: "chart.xyaxis.line",
+              pointSize: 24,
+            }}
+          ></DropdownMenu.ItemIcon>
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+    </DropdownMenu.Root>
+  );
+};
+
+export default MoreButton;
