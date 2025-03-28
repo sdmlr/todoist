3b1ce88b2392ae9c7363d2c8e80cad63d15e5d50
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-28 02:25:03 +0000

    adds outer layout

---
+++ app/(authenticated)/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{ contentStyle: { backgroundColor: "#fff" } }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="task/new" options={{ presentation: 'modal' }} />
+      </Stack>
+  );
+};
+
+export default Layout;
