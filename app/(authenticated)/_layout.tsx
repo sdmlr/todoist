d3898f3b9c255bc3be25dc9930196863e99ea394
Stefano Di Michele <sdmlr_@hotmail.com>
2025-03-30 16:07:42 +0100

    prettier

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -6,9 +6,9 @@ const Layout = () => {
   return (
     <Stack screenOptions={{ contentStyle: { backgroundColor: "#fff" } }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="task/new" options={{ presentation: 'modal' }} />
-      <Stack.Screen name="task/[id]" options={{ presentation: 'modal' }} />
-      </Stack>
+      <Stack.Screen name="task/new" options={{ presentation: "modal" }} />
+      <Stack.Screen name="task/[id]" options={{ presentation: "modal" }} />
+    </Stack>
   );
 };
 
