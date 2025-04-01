d401ad3b5c5f2bdbd21d7c7f0f36fb301f351cdb
Stefano Di Michele <sdmlr_@hotmail.com>
2025-04-01 21:51:10 +0100

    wrapped with rcat

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,68 +1,71 @@
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Colors } from "@/constants/Colors";
+import { RevenueCatProvider } from "@/providers/RevenueCat";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: Colors.primary,
-        tabBarInactiveTintColor: Colors.dark
-      }}
-    >
-      <Tabs.Screen
-        name="today"
-        options={{
-          title: "Today",
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons
-              name={focused ? "calendar" : "calendar-outline"}
-              size={size}
-              color={color}
-            />
-          ),
+    <RevenueCatProvider>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: Colors.primary,
+          tabBarInactiveTintColor: Colors.dark,
         }}
-      />
-      <Tabs.Screen
-        name="upcoming"
-        options={{
-          title: "Upcoming",
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons
-              name={focused ? "time" : "time-outline"}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: "Search",
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons
-              name={focused ? "search" : "search-outline"}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="browse"
-        options={{
-          title: "Browse",
-          tabBarIcon: ({ focused, color, size }) => (
-            <Ionicons
-              name={focused ? "document-text" : "document-text-outline"}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="today"
+          options={{
+            title: "Today",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons
+                name={focused ? "calendar" : "calendar-outline"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="upcoming"
+          options={{
+            title: "Upcoming",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons
+                name={focused ? "time" : "time-outline"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            title: "Search",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons
+                name={focused ? "search" : "search-outline"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="browse"
+          options={{
+            title: "Browse",
+            tabBarIcon: ({ focused, color, size }) => (
+              <Ionicons
+                name={focused ? "document-text" : "document-text-outline"}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </RevenueCatProvider>
   );
 }
