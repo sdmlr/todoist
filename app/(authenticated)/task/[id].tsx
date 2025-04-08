ae3fb6f95f33bc37561ffa30926228c7e82be2ba
Stefano Di Michele <sdmlr_@hotmail.com>
2025-04-08 15:49:10 +0100

    make task screen functional

--- app/(authenticated)/task/[id].tsx
+++ app/(authenticated)/task/[id].tsx
@@ -1,13 +1,35 @@
-import { View, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { useSQLiteContext } from "expo-sqlite";
+import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { projects, todos } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import TodoForm from "./new";
 
 const Page = () => {
   const { id } = useLocalSearchParams();
-  return (
-    <View>
-      <Text>Page: {id}</Text>
-    </View>
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db);
+  const { data } = useLiveQuery(
+    drizzleDb
+      .select()
+      .from(todos)
+      .where(eq(todos.id, Number(id)))
+      .leftJoin(projects, eq(todos.project_id, projects.id))
   );
+
+  if (!!data && data.length === 0) {
+    return null;
+  }
+
+  const todo = {
+    ...data[0].todos,
+    project_name: data[0].projects?.name ?? '',
+    project_color: data[0].projects?.color ?? '',
+  };
+
+  console.log("Page - todo:", todo);
+
+  return <TodoForm todo={todo} />;
 };
 
 export default Page;
